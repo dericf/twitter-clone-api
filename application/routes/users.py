72a83c859fa77ed1d79e6fdcfda4509b2cd253d1
deric92 <dericgf@gmail.com>
2020-12-02 19:03:58 -0700

    Users API

--- application/routes/users.py
+++ application/routes/users.py
@@ -1,5 +1,8 @@
-from flask import app, request, jsonify
-from __main__ import app, db, utils
+from flask import request, jsonify
+from application import app
+
+import db
+import utils
 
 
 @app.route('/api/users', methods=['GET'])
@@ -9,28 +12,28 @@ def users_get():
         response_data = []
         if user_id:
             success, user = db.get_one(
-                "SELECT id as user_id, email, username, bio FROM users WHERE id=?", (int(user_id),))
+                "SELECT id as user_id, email, username, bio, birthdate FROM users WHERE id=?", (int(user_id),))
             print(list(user))
             response_data.append({
                 "userId": user[0],
                 "email": user[1],
                 "username": user[2],
                 "bio": user[3],
-                "birthdate": "0000-00-00"
+                "birthdate": user[4]
 
             })
 
             return jsonify(response_data) if success else {"STATUS": "ERROR"}, 500
         else:
             success, users = db.get_all(
-                "SELECT id as user_id, email, username, bio FROM users")
+                "SELECT id as user_id, email, username, bio, birthdate FROM users")
             for user in users:
                 response_data.append({
                     "userId": user[0],
                     "email": user[1],
                     "username": user[2],
                     "bio": user[3],
-                    "birthdate": "0000-00-00"
+                    "birthdate": user[4]
                 })
             return jsonify(response_data) if success else {"STATUS": "ERROR"}, 500
 
@@ -42,10 +45,11 @@ def users_post():
         password = request.json.get('password')
         username = request.json.get('username')
         bio = request.json.get('bio')
+        birthdate = request.json.get('birthdate')
 
         p_hash = utils.hash_password(password)
         status = db.insert_one(
-            "INSERT INTO users (email, password_hash, username, bio) VALUES(?,?,?,?)", (email, p_hash, username, bio))
+            "INSERT INTO users (email, password_hash, username, bio, birthdate) VALUES(?,?,?,?,?)", (email, p_hash, username, bio, birthdate))
         print(status)
         if status:
             success, user = db.get_user_by_email(email)
@@ -55,7 +59,7 @@ def users_post():
                 "email": user[2],
                 "bio": user[3],
                 "birthdate": "000-00-00",
-                "loginToken": utils.generate_login_token(user[0])
+                "loginToken": utils.generate_login_token(user[0], user[1])
             }
 
             return return_data
