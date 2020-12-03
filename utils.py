acd2a4256a717fe35957c51212af26418d4295ba
deric92 <dericgf@gmail.com>
2020-12-02 19:07:14 -0700

    Add generic utility functions

--- utils.py
+++ utils.py
@@ -13,14 +13,20 @@ def check_password(raw, hashed):
     return bcrypt.checkpw(raw.encode('UTF-8'), hashed)
 
 
-def generate_login_token(userId):
-    return (jwt.encode({'userId': userId, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, current_app.config['SECRET_KEY'])).decode('UTF-8')
+def generate_login_token(userId, username):
+    return (jwt.encode({'userId': userId, "username": username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, current_app.config['SECRET_KEY'])).decode('UTF-8')
 
 
 def decode_login_token(token):
+    """Returns the decoded JWT as a dictionary"""
     try:
-        decoded = jwt.decode(token, current_app.config["SECRET_KEY"])
+        decoded = jwt.decode(token, current_app.config["SECRET_KEY"], verify=False)
         return decoded
     except jwt.ExpiredSignatureError:
         print("Token is expired.")
+        if current_app.config["DEBUG"]:
+            # Bypass Expiry for development
+            return decoded
         return False
+    except Exception as e:
+        print("JWT Error", e)
