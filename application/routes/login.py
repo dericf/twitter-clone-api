3c4523f402d36f73b74a6a5cb6b246f63a394ecd
deric92 <dericgf@gmail.com>
2020-12-02 21:07:02 -0700

    Improve Login API

--- application/routes/login.py
+++ application/routes/login.py
@@ -8,26 +8,37 @@ def login_post():
     if request.method == 'POST':
         email = request.json.get('email')
         password = request.json.get('password')
+        #
+        # Try and get the user
+        #
         success, user = db.get_user_by_email(email)
-        login_token = utils.generate_login_token(user[0], user[1])
-        if user:
-          if utils.check_password(password, user[4]):
-              # sucess
-              status = db.insert_one(
-                  "INSERT INTO sessions (user_id, token) VALUES(?,?)", (user[0], login_token))
-              print(status)  
-              return_data = {
-                  "userId": user[0],
-                  "username": user[1],
-                  "email": user[2],
-                  "bio": user[3],
-                  "birthdate": "000-00-00",
-                  "loginToken": login_token
-              }
+        print("User", user)
 
-              return return_data, 201
-              
-        return {"STATUS": "ERROR"}, 500
+        if not user:
+            return jsonify({"status": "error", "message":"No user with that email exists"}), 500
+        #
+        # Check if passwords match
+        #
+        if not utils.check_password(password, user[4]):
+            return jsonify({"status": "error", "message":"Passwords do not match"}), 500
+        #
+        # Generate the  login token
+        #
+        login_token = utils.generate_login_token(user[0], user[1])
+        print("login token", type(login_token))
+        status = db.insert_one(
+            "INSERT INTO sessions (user_id, token) VALUES(?,?)", (user[0], login_token))
+        
+        return_data = {
+            "userId": user[0],
+            "username": user[1],
+            "email": user[2],
+            "bio": user[3],
+            "birthdate": user[5],
+            "loginToken": login_token
+        }
+
+        return jsonify(return_data), 201
 
 
 @app.route('/api/login', methods=['DELETE'])
@@ -36,10 +47,10 @@ def login_delete():
         encoded_login_token = request.json.get("loginToken")
         login_token = utils.decode_login_token(encoded_login_token)
         user_id = login_token['userId']
-        print(login_token, user_id)
+        
         # delete the session
         if user_id:
             db.delete_one("DELETE FROM sessions WHERE token=? and user_id=?", (encoded_login_token, user_id))
-            return "<pre>{}</pre>", 204
+            return "", 204
         else:
-            return {"STATUS": "ERROR"}, 500
+            return {"status": "error"}, 500
